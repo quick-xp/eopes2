b222f757d9f1de311089775d85fc954ab2561449
r_nishio <quick.xp@me.com>
2016-04-30 12:07:04 +0900

    estimate job cost修正

--- app/models/estimate_job_cost.rb
+++ app/models/estimate_job_cost.rb
@@ -19,22 +19,22 @@ class EstimateJobCost < ActiveRecord::Base
   belongs_to :estimate
   attr_accessor :blueprint_type_id, :blueprint_runs
 
-  def re_calc_job_cost!(material_list, runs)
+  def calc_job_cost!
     #SystemCostIndex設定
     self.system_cost_index =
-      EstimateJobCost.get_system_cost_index(self.region_id, self.solar_system_id)
+      EstimateJobCost.calc_system_cost_index(self.region_id, self.solar_system_id)
 
     #BaseJobCost設定
-    self.base_job_cost = get_base_job_cost(material_list, runs)
+    self.base_job_cost = calc_base_job_cost(self.blueprint_type_id, self.blueprint_runs)
 
     #JobFee設定
-    self.job_fee = get_job_fee
+    self.job_fee = calc_job_fee(self.system_cost_index, self.base_job_cost)
 
     #FacilityCost設定
-    self.facility_cost = get_facility_cost
+    self.facility_cost = calc_facility_cost(self.system_cost_index, self.base_job_cost)
 
     #TotalJobInstallCost設定
-    self.total_job_cost = get_total_job_cost
+    self.total_job_cost = calc_total_job_cost(self.job_fee, self.facility_cost)
   end
 
   def initialize(blueprint_type_id, blueprint_runs, region_id = "", solar_system_id = "")
@@ -48,7 +48,7 @@ class EstimateJobCost < ActiveRecord::Base
   #region_id,solar_system_idどちらも指定されていない場合は全Cost_Indexの平均を
   #region_idのみ指定の場合は、Region内のCost_Indexの平均を
   #solar_system_idまで指定している場合はSolarSystemのCost_Indexを取得する
-  def self.get_system_cost_index(region_id, solar_system_id)
+  def self.calc_system_cost_index(region_id, solar_system_id)
     if region_id.nil?
       region_id = ""
     end
@@ -82,31 +82,31 @@ class EstimateJobCost < ActiveRecord::Base
 
   #BaseJobCost計算
   #Σ( baseQuantity * adjustedPrice)
-  def base_job_cost
+  def calc_base_job_cost(blueprint_type_id, blueprint_runs)
     base_job_cost = 0.0
-    materials = IndustryActivityMaterial.materials_for_production_job(self.blueprint_type_id)
+    materials = IndustryActivityMaterial.materials_for_production_job(blueprint_type_id)
     materials.each do |material|
       base_job_cost += material.base_quantity * material.adjusted_price
     end
-    base_job_cost * self.blueprint_runs
+    base_job_cost * blueprint_runs
   end
 
   #JobFee計算
   #system_cost_index * baseJobCost
-  def job_fee
-    self.system_cost_index * self.base_job_cost
+  def calc_job_fee(system_cost_index, base_job_cost)
+    system_cost_index * base_job_cost
   end
 
   #FacilityCost計算
   #JobFee * taxRate / 100
-  def facility_cost
-    self.job_fee * 10 / 100
+  def calc_facility_cost(job_fee, tax_rate = 10)
+    job_fee * tax_rate / 100
   end
 
   #TotalJobCost計算
   #JobFee + FacilityCost
-  def total_job_cost
-    self.job_fee + self.facility_cost
+  def calc_total_job_cost(job_fee, facility_cost)
+    job_fee + facility_cost
   end
 
 end
