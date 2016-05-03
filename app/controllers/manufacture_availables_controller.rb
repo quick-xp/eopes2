e7199447b442e39d7945091dc67d760139d8c3b5
r_nishio <quick.xp@me.com>
2016-05-04 01:47:54 +0900

    製造可能設計図について 検索ワードを指定できるように修正

--- app/controllers/manufacture_availables_controller.rb
+++ app/controllers/manufacture_availables_controller.rb
@@ -2,7 +2,7 @@ class ManufactureAvailablesController < ApplicationController
   include ActionController::Serialization
 
   def index
-    @availables = IndustryActivity.manufacture_availables
+    @availables = IndustryActivity.manufacture_availables(params[:searchWord]).limit(100)
     render json: @availables
   end
 end
--- app/models/master/industry_activity.rb
+++ app/models/master/industry_activity.rb
@@ -12,10 +12,11 @@ class IndustryActivity < ActiveRecord::Base
   establish_connection(:EveMasterDB)
   self.primary_key = :typeID
 
-  def self.manufacture_availables
+  def self.manufacture_availables(search_word)
     IndustryActivity.joins("inner join invTypes on industryActivity.typeID = invTypes.typeID")
                     .where(activityID: 1)
                     .where("invTypes.typeName NOT LIKE '%YC117%'")
+                    .where("invTypes.typeName LIKE '%#{search_word}%'")
                     .select("invTypes.typeID,invTypes.typeName")
                     .order("invTypes.typeName asc")
   end
